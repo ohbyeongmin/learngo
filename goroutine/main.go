22b7d2e7be6cdccbb1aaf8220fe739d14b3b73da
ohbyeongmin <fbottlem@gmail.com>
2021-06-07 19:23:54 +0900

    goroutine channel hiturl

---
+++ goroutine/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main(){
+	c := make(chan string)
+	people := [5]string{"byeongmin", "solhui", "a", "b", "c"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	
+	for i:=0;i<len(people);i++ {
+		fmt.Print("Waiting for: ", i)
+		fmt.Println(<-c)
+	}
+}
+
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 5)
+	c <- person + " is sexy"
+}
--- main.go
+++ main.go
@@ -2,54 +2,49 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/ohbyeongmin/learngo/mydict"
+	"net/http"
 )
 
+type requestResult struct {
+	url string
+	status string
+}
+
 func main(){
-	// account := accounts.NewAccount("obm")
-	// account.Deposit(10)
-	
-	// fmt.Println(account)
 
-	// dictionary := mydict.Dictionary{"first": "First word"}
-	// definition, err := dictionary.Search("second")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// } else {
-	// 	fmt.Println(definition)
-	// }
-
-	// dictionary := mydict.Dictionary{}
-	// err := dictionary.Add("hello", "Greeting")
-
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// definition, err2 := dictionary.Search("hello")
-	// if err2 != nil {
-	// 	fmt.Println(err2)
-	// }
-	// fmt.Println(definition)
-
-	// dictionary := mydict.Dictionary{}
-	// baseWord := "hello"
-	// dictionary.Add(baseWord, "First")
-	// err := dictionary.Update(baseWord, "Second")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// word, _ := dictionary.Search(baseWord);
-	// fmt.Println(word)
-
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
+	results := map[string]string{}
+	c := make(chan requestResult)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	fmt.Println(word)
+
+	for _, url := range urls{	
+		go hitURL(url, c)
+	}
+
+	for i:=0;i<len(urls);i++ {
+		result :=  <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results{
+		fmt.Println(url, status)
+	}
+}
+
+func hitURL(url string, c chan<- requestResult) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILD"
+	}
+	c <- requestResult{url:url, status: status}
 }
